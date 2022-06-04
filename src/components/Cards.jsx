a20975ea34c87d399e0fda86761d5b7dd915cce3
Reta <r.range755@gmail.com>
2022-06-04 01:40:36 -0300

    create component cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import cities from '../data';
 import Card from './Card';
 
 
 
 
-export default function Cards(props) {
+export default function Cards({cities}) {
   // acá va tu código
   // tip, podés usar un map
-  return (
-    <div>
-        <Card />       
-    </div>
-    )
+	return (
+		<div>
+			{cities.map((city) => (
+				<Card
+					key={city.id}
+					min={city.main.temp_min} 
+					max={city.main.temp_max} 
+					name={city.name} 
+					img={city.weather[0].icon} 
+					onClose={() => alert(city.name)}
+					/>
+				))}
+		</div>
+		)
 };
