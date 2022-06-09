56ef948d65e42dc453d660c1c155747ca878b44b
Reta <r.range755@gmail.com>
2022-06-09 03:41:02 -0300

    actualizar iconos

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,6 +4,20 @@ import PropTypes from 'prop-types';
 import {IoCloseCircleOutline} from 'react-icons/io5';
 import icon_09d from '../../assets/09d.png'
 import icon_03n from '../../assets/03n.png'
+import icon_01d from '../../assets/01d.png'
+import icon_02d from '../../assets/02d.png'
+import icon_04d from '../../assets/04d.png'
+import icon_10d from '../../assets/10d.png'
+import icon_11d from '../../assets/11d.png'
+import icon_13d from '../../assets/13d.png'
+import icon_50d from '../../assets/50d.png'
+import icon_01n from '../../assets/01n.png'
+import icon_02n from '../../assets/02n.png'
+import icon_09n from '../../assets/09n.png'
+import icon_11n from '../../assets/11n.png'
+import icon_13n from '../../assets/13n.png'
+
+
 
 import styles from './Card.module.css';
 
@@ -30,8 +44,24 @@ export default function Card({max, min, name, img, onClose}) {
 };
 
 function WeatherIcon({icon}) {
-	switch(icon) {
-		case "03n": return <img src={icon_03n} alt="shower rain"/>;
+	switch(icon) {		
+		case "01d": return <img src={icon_01d} alt="clear sky"/>;
+		case "02d": return <img src={icon_02d} alt="few clouds"/>;
+		case "03d": return <img src={icon_03n} alt="scattered clouds"/>;
+		case "04d": return <img src={icon_04d} alt="broken clouds"/>;
+		case "09d": return <img src={icon_09d} alt="shower rain"/>;
+		case "10d": return <img src={icon_10d} alt="rain"/>;
+		case "11d": return <img src={icon_11d} alt="thunderstorm"/>;
+		case "13d": return <img src={icon_13d} alt="snow"/>;
+		case "50d": return <img src={icon_50d} alt="mist"/>;
+		case "01n": return <img src={icon_01n} alt="clear sky"/>;
+		case "02n": return <img src={icon_02n} alt="few clouds"/>;
+		case "03n": return <img src={icon_03n} alt="scattered clouds"/>;
+		case "04n": return <img src={icon_04d} alt="broken clouds"/>;
+		case "09n": return <img src={icon_09n} alt="shower rain"/>;
+		case "11n": return <img src={icon_11n} alt="thunderstorm"/>;
+		case "13n": return <img src={icon_13n} alt="snow"/>;
+		case "50n": return <img src={icon_50d} alt="mist"/>;
 		default: 
 			return <img src={icon_09d} alt="shower rain"/>;
 	}
